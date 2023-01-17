be6dba3d5b32d66c978634da5010b0e4ecf7c16e
swarajpeppermint <swaraj@getpeppermint.co>
2023-01-17 14:23:45 +0530

    Updated Licensing Information

--- src/map2world/setup.py
+++ src/map2world/setup.py
@@ -4,7 +4,7 @@ package_name = 'map2world'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,8 +15,8 @@ setup(
     zip_safe=True,
     maintainer='swaraj',
     maintainer_email='swaraj@getpeppermint.co',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='map2world: Convert 2D map into a 3D world',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
